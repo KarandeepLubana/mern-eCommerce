abfb6c18d689952f25a1521b76d904943de59249
Karandeep Lubana <karandeepLubana1@gmail.com>
2021-12-28 20:16:01 -0500

    fetch products from database and implemented data seeder script

---
+++ backend/seeder.js
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const users = require("./data/users");
+const products = require("./data/products");
+// const users = require("./data/users");
+const User = require("./models/userModels");
+const Product = require("./models/productModels");
+const Order = require("./models/orderModel");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
+
+const importData = async () => {
+  try {
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    const createdUsers = await User.insertMany(users);
+    console.log(createdUsers)
+
+    const adminUser = createdUsers[0]._id;
+
+    const sampleProducts = products.map((product) => {
+      return { ...product, user: adminUser };
+    });
+
+
+
+    await Product.insertMany(sampleProducts);
+
+    
+    console.log("Data Imported!".green.inverse);
+    process.exit();
+  } catch (error) {
+      console.log('~Error~')
+    console.log(`${error}`.red.inverse);
+    process.exit(1);
+  }
+};
+
+const destroyData = async () => {
+  try {
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    console.log("Data Destroyed!".red.inverse);
+    process.exit();
+  } catch (error) {
+    console.log(`${errpr}`.red.inverse);
+    process.exit(1);
+  }
+};
+
+
+if (process.argv[2] === '-d') {
+    destroyData();
+} else {
+    importData();
+}
+
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const products = require("./data/products");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const colors = require("colors");
+const productRoutes = require("./routes/productRoutes");
 
 dotenv.config();
 
@@ -14,17 +14,12 @@ app.get("/", (req, res) => {
   res.send("API is running ...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((product) => product._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
