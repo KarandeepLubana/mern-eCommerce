74913de7be3bc3367bb7cac95189f55234d573ee
Karandeep Lubana <karandeepLubana1@gmail.com>
2022-01-01 17:47:15 -0500

    Authentication middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,40 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModels");
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  //   console.log(req.headers.authorization);
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    // console.log("token found");
+
+    try {
+      // console.log("INSIDE 1");
+      token = req.headers.authorization.split(" ")[1];
+      // console.log("INSIDE 2", token, process.env.JWT_SECRET);
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = await User.findById(decoded.id).select("-password"); // req.user will be accessible to all our protected routes
+
+      // next();
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+
+  next();
+});
+
+module.exports = protect;
