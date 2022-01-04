0b362a436d625c131af67ef34041c04293d5665e
Karandeep Lubana <karandeepLubana1@gmail.com>
2022-01-03 23:46:12 -0500

    Added PayPal payments and showing orders on profile screen

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,11 +3,15 @@ const router = express.Router();
 const {
   addOrderItems,
   getOrderById,
+  updateOrderToPaid,
+  getMyOrders,
 } = require("../controllers/orderController");
 const protect = require("../middleware/authMiddleware");
 
 // router.post('/login', authUser);
+router.get("/myorders", [protect, getMyOrders]);
 router.post("/", [protect, addOrderItems]);
 router.get("/:id", [protect, getOrderById]);
+router.put("/:id/pay", [protect, updateOrderToPaid]);
 
 module.exports = router;
--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Link, Navigate } from "react-router-dom";
 import {
   Button,
@@ -13,19 +14,26 @@ import { useDispatch, useSelector } from "react-redux";
 import { saveShippingAddress } from "../actions/cartActions";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
-import { getOrderDetails } from "../actions/orderActions";
+import { getOrderDetails, payOrder } from "../actions/orderActions";
 import { getUserDetails } from "../actions/userActions";
 import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
+import { PayPalButton } from "react-paypal-button-v2";
+import { ORDER_PAY_RESET } from "../constants/orderConstants";
 
 const OrderScreen = () => {
   const dispatch = useDispatch();
+
+  const [sdkReady, setSdkReady] = useState(false);
 
   let navigate = useNavigate();
   const { id: orderId } = useParams();
 
   const orderDetails = useSelector((state) => state.orderDetails);
   const { order, loading, error } = orderDetails;
+
+  const orderPay = useSelector((state) => state.orderPay);
+  const { loading: loadingPay, success: successPay } = orderPay;
 
   const userDetails = useSelector((state) => state.userDetails);
   const { user } = userDetails;
@@ -41,12 +49,37 @@ const OrderScreen = () => {
   }
 
   useEffect(() => {
-    if (!order || order._id !== orderId) {
+    // Dynamically add the PayPall Script
+    const addPayPalScript = async () => {
+      const { data: clientId } = await axios.get("/api/config/paypal");
+      const script = document.createElement("script");
+      script.type = "text/javascript";
+      script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}`;
+      script.async = true;
+      script.onload = () => {
+        setSdkReady(true);
+      };
+      document.body.appendChild(script);
+    };
+
+    if (!order || successPay) {
+      dispatch({ type: ORDER_PAY_RESET });
       dispatch(getOrderDetails(orderId));
+    } else if (!order.isPaid) {
+      if (!window.paypal) {
+        addPayPalScript();
+      } else {
+        setSdkReady(true);
+      }
     }
 
     dispatch(getUserDetails("profile"));
-  }, [order, orderId]);
+  }, [dispatch, orderId, successPay, order]);
+
+  const successPaymentHandler = (paymentResult) => {
+    console.log(paymentResult);
+    dispatch(payOrder(orderId, paymentResult));
+  };
 
   return loading ? (
     <Loader />
@@ -162,6 +195,20 @@ const OrderScreen = () => {
                   <Col>${order.totalPrice}</Col>
                 </Row>
               </ListGroup.Item>
+
+              {!order.isPaid && (
+                <ListGroup.Item>
+                  {loadingPay && <Loader />}
+                  {!sdkReady ? (
+                    <Loader />
+                  ) : (
+                    <PayPalButton
+                      amount={order.totalPrice}
+                      onSuccess={successPaymentHandler}
+                    />
+                  )}
+                </ListGroup.Item>
+              )}
             </ListGroup>
           </Card>
         </Col>
