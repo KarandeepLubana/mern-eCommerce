855797be937905fefc443f862aae3436bdd191b1
Karandeep Lubana <karandeepLubana1@gmail.com>
2022-01-05 23:51:00 -0500

    Prepare for Deployment

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,6 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.json()); // parses json data in the body
 
-app.get("/", (req, res) => {
-  res.send("API is running ...");
-});
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -37,6 +33,18 @@ app.get("/api/config/paypal", (req, res) =>
 
 const dirname = path.resolve();
 app.use("/uploads", express.static(path.join(dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running ...");
+  });
+}
 
 app.use(notFound);
 
