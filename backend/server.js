4ff711eb9f7a7e4e0efccb68b13b2c7ade821553
Karandeep Lubana <karandeepLubana1@gmail.com>
2022-01-05 23:27:41 -0500

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,13 @@ const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-const colors = require("colors");
 const productRoutes = require("./routes/productRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const userRoutes = require("./routes/userRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 const uploadRoutes = require("./routes/uploadRoutes");
 const morgan = require("morgan");
+const colors = require("colors");
 
 dotenv.config();
 
@@ -22,10 +22,6 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.json()); // parses json data in the body
 
-app.get("/", (req, res) => {
-  res.send("API is running ...");
-});
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -34,9 +30,19 @@ app.use("/api/upload", uploadRoutes);
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
-
 const dirname = path.resolve();
 app.use("/uploads", express.static(path.join(dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(dirname, "/frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running ...");
+  });
+}
 
 app.use(notFound);
 
